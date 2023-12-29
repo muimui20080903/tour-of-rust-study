314ef05ef26526522529d65178e3db77ef1a879c
muimui20080903 <98527235+muimui20080903@users.noreply.github.com>
2023-12-29 15:42:59 +0900

    complete chapter4!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tour-of-rust-study
-[Rustツアー](https://tourofrust.com/00_ja.html)をやってみる
+[Rustツアー](https://tourofrust.com/TOC_ja.html)をやってみる
---
+++ chapter3/24.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // let s = "Hello world!";
+    let s = String::from("Hello world!");
+    println!("{} is {} characters long", s, s.len());
+}
